c9ca9bc2c9cc6120654f8c29e1687188d0084383
vagrant <vagrant@example.com>
2021-06-21 12:49:21 +0200

    Mqtt Test

---
+++ controls/tests.rb
@@ -0,0 +1,56 @@
+lxc_machines = input('lxc_mqtt')
+#puts input_object('lxc_mqtt').diagnostic_string
+
+control 'machines' do
+  impact 1.0
+  title 'Erstellen der LXC Maschinen'
+  desc 'Erstellen der Maschine'
+  lxc_machines.each do |machine|
+    describe bash('sudo lxc-ls') do
+      # Gefolgt von einem Whitespace
+      its('stdout') { should match /#{machine[:'name']}\s+/ }
+    end
+    if bash('sudo lxc-ls --running | grep ' + machine[:'name']).exit_status != 0
+      describe bash('sudo lxc-start --name ' + machine[:'name']) do
+        its('exit_status') { should cmp 0 }
+      end
+    end
+  end
+end
+
+control 'packages' do
+  impact 1.0
+  title 'Erstellen der LXC Maschinen'
+  desc 'Erstellen der Maschine'
+  lxc_machines.each do |machine|
+    describe package('mosquitto') do
+      it { should be_installed }
+    end
+  end
+end
+
+control 'ports' do
+  impact 1.0
+  title 'Erstellen der LXC Maschinen'
+  desc 'Erstellen der Maschine'
+  lxc_machines.each do |machine|
+    describe port(1883) do
+      it { should be_listening }
+      its('protocols') {should eq ['tcp', 'tcp6']}
+      its('addresses') {should eq ['0.0.0.0', '::']}
+    end
+  end
+end
+
+control 'service' do
+  impact 1.0
+  title 'Erstellen der LXC Maschinen'
+  desc 'Erstellen der Maschine'
+  lxc_machines.each do |machine|
+    describe service('mosquitto') do
+      it { should be_installed }
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+end
